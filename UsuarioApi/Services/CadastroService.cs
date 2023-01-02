52dbdc650084566bb144538472b49d95234416c9
welligton_fadami <welligtoncos@gmail.com>
2023-01-02 17:36:07 -0300

    bloqueando criacao de user existente

--- UsuarioApi/Services/CadastroService.cs
+++ UsuarioApi/Services/CadastroService.cs
@@ -30,22 +30,27 @@ namespace UsuarioApi.Services
         [Obsolete]
         public Result CadastroUsuario(CreateUsuarioDto createDto)
         {
-            var usuario = _mapper.Map<Usuario>(createDto);
-            CustomIdentityUser usuarioIdentity = _mapper.Map<CustomIdentityUser>(usuario);
-            var resultadoIdentity =  _userManager
-                .CreateAsync(usuarioIdentity, createDto.Password);
-             
-
-            if (resultadoIdentity.Result.Succeeded)
+            //Existe usuario?
+            var identityUser = _userManager
+               .Users
+               .FirstOrDefault(u => u.UserName == createDto.Username);
+            if (identityUser == null)
             {
-                _userManager.AddToRoleAsync(usuarioIdentity, "regular"); 
+                var usuario = _mapper.Map<Usuario>(createDto);
+                CustomIdentityUser usuarioIdentity = _mapper.Map<CustomIdentityUser>(usuario);
+                var resultadoIdentity = _userManager
+                    .CreateAsync(usuarioIdentity, createDto.Password);
 
-                var code = _userManager.GenerateEmailConfirmationTokenAsync(usuarioIdentity).Result;
-                var encodedCode = HttpUtility.UrlEncode(code);
-                _emailService.EnviarEmail(new[] { usuarioIdentity.Email}, "Link de Ativação", usuarioIdentity.Id, encodedCode);
-                return Result.Ok().WithSuccess(code).WithSuccess(encodedCode);
-            }
-            
+                if (resultadoIdentity.Result.Succeeded)
+                {
+                    _userManager.AddToRoleAsync(usuarioIdentity, "regular");
+
+                    var code = _userManager.GenerateEmailConfirmationTokenAsync(usuarioIdentity).Result;
+                    var encodedCode = HttpUtility.UrlEncode(code);
+                    _emailService.EnviarEmail(new[] { usuarioIdentity.Email }, "Link de Ativação", usuarioIdentity.Id, encodedCode);
+                    return Result.Ok().WithSuccess(code).WithSuccess(encodedCode);
+                }
+            } 
             return Result.Fail("Falha ao cadastrar usuário");
         }
 
